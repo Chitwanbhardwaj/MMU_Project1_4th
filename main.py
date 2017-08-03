8ea64d0b8ac5bb77e06fb65cf222a52fb1b45bb6
chitwan bhardwaj <chitwanbhardwaj2@gmail.com>
2017-08-03 17:59:18 +0530

    spy ID Created

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import time
-print "****"
+print "****SPY SEE****"
 spy_name = raw_input("Enter your assigned name here.")
 # '+' denotes the process of concatination.
 spy_salutation = raw_input("What should we call you (Mr. or Ms.)?")
 # Needs to modify the above code..
 if len(spy_name) > 0 :
     print 'Input Taken'
+
 spy_name = spy_salutation + " " + spy_name
 # parsing take place in above command, reading right to left.
 # considering the priority of operators from right to left.
@@ -16,3 +17,4 @@ done = time.time()
 # Need Time interval for the above code
 print "User Identified."
 print "welcome\t" + spy_name
+print "Alright\t" + spy_name + "\nBefore proceeding, Lets talk little more about you."
